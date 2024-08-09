5d50d0b2e44596afe441c94f30d288af575a5f2f
Olorunfemi Olamide <166822505+BigOTF@users.noreply.github.com>
2024-08-09 11:21:17 +0100

    Started working on the Filter by category

--- src/components/Sales/Sales.jsx
+++ src/components/Sales/Sales.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import CountdownTimer from './Countdown';
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
 import StarRating from './StarRating';
+import { FaStar } from "react-icons/fa";
 /* https://api.escuelajs.co/api/v1/products */
 /* https://fakestoreapi.com/products */
 
@@ -12,34 +13,41 @@ const Sales = () => {
     useEffect(() => {
         const fetchItems = async() => {
             try {
-                const response = await fetch(' https://fakestoreapi.com/products');
+                const response = await fetch('https://api.escuelajs.co/api/v1/products');
                 const theData = await response.json();
                 console.log(theData);
                 return setProduct(theData);
+              
             } catch(err) {
                 console.log(err.message);
             }
         }
-        fetchItems();
+        (async() => await fetchItems())();
     }, []);
+
+    const StarData = () => {
+        return (
+            [...Array(5)].map((star) => {
+                return <FaStar className='w-[20px] h-[20px] text-[#FFAD33]'/>
+            })
+        )
+    }
 
     const ProductData = () => {
         return  product.map((data) => {
-          
             return (
                 <div key={data.id} className='border w-[270px] h-[350px]'>
-
                     <div className='w-[270px] h-[230px] bg-[#F5F5F5] flex items-center justify-center'>
-                        <img src={data.image} className='h-[180px] w-[190px] bg-[#F5F5F5] object-contain object-center bg-inherit'/>
-                    </div>      
-                    <div className='flex flex-col h-[104px] w-[201px] justify-between mt-4'>
+                        <img src={data.image} className='h-[180px] w-[190px] bg-[#F5F5F5]'/>
+                    </div>     
+
+                    <div className='flex flex-col h-[104px] w-[201px] mt-4 gap-1'>
                         <p className='text-[16px] font-medium'>{data.title}</p>
                         <p className='text-[#DB4444] text-[16px] font-medium'>{`$`}{data.price}</p>
-                        <StarRating rating={data.rating} maxStars={10} />
                     </div>
                 </div>
             )
-        }).slice(0, 3);
+        })
     }
 
   return (
